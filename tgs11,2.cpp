0259f38c321b2df93e6d457fcb10b07f4186a079
kautsarapp <113043834+kautsarapp@users.noreply.github.com>
2023-01-11 12:37:04 +0700

    Add files via upload

---
+++ tgs11,2.cpp
@@ -0,0 +1,8 @@
+Line 1 – 3 = Header Program.
+Line 5 = Fungsi utama.
+Line 6 = Deklarasi variable pointer “P”.
+Line 7 = Mencoba mengalokasikan dan menginisialisasi objek atau dari jenis tempat penampung atau tertentu, dan mengembalikan penunjuk tipe bukan nol yang sesuai ke objek.
+Line 9 = Menyatakan bahwa pointer P bernilai 100.
+Line 11 = Perintah print “Nilai *P : “ dan print variable pointer P.
+Line 12 = Membatalkan alokasi blok memori variable P.
+Line 13 = Mengembalikan nilai 0.
